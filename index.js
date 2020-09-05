a5083c8986b2911dd13306f065f81b00eecbacc1
UltraaaDev <36534987+UltraaaDev@users.noreply.github.com>
2020-09-06 00:15:02 +0100

    remove keypress check

--- index.js
+++ index.js
@@ -51,15 +51,4 @@ var textChange = () => {
 
 $('document').ready(() => {
     document.getElementById("calc").onclick = textChange;
-
-    function isNumberKey(evt) {
-        var charCode = (evt.which) ? evt.which : evt.keyCode;
-        if (charCode != 46 && charCode > 31 &&
-            (charCode < 48 || charCode > 57))
-            return false;
-
-        return true;
-    }
-
-    $('textarea').keypress(isNumberKey)
 });
